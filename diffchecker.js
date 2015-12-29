580c805a279e94fb473e1670c6eafd67845d5bd9
OKNoah <noahgray@me.com>
2015-12-28 16:46:16 -0800

    Adds user authentication and sign in.
    Asks users to sign in to use app.

--- diffchecker.js
+++ diffchecker.js
@@ -1,32 +1,82 @@
 import request from 'superagent';
 import fs from 'fs';
 import opener from 'opener';
+import prompt from 'prompt';
+
+const API_URI = 'https://diffchecker-api-production.herokuapp.com';
 
 if (process.argv.length !== 4) {
-  console.log("There should be 2 arguments. There are only " + (process.argv.length - 1) + ".\n");
+  console.log('There should be 2 arguments. There are only ' + (process.argv.length - 1) + '.\n');
   process.exit(1);
+}
+
+function authorization () {
+  return new Promise((resolve, reject) => {
+    try {
+      const config = require('./diffchecker.json');
+      resolve(config);
+    } catch (err) {
+      console.log("You don't appear to be logged in. Sign up for a free account at https://www.diffchecker.com/signup and enter your credentials.");
+      prompt.start();
+      prompt.get({
+        properties: {
+          email: {
+            required: true,
+            message: 'Email'
+          },
+          password: {
+            required: true,
+            message: 'Password',
+            hidden: true
+          }
+        }
+      }, (error, result) => {
+        request
+          .post(API_URI + '/sessions')
+          .send(result)
+          .end((er, response) => {
+            if (er) reject(er);
+
+            const config = { authToken: response.body.authToken };
+
+            fs.writeFile('./diffchecker.json', JSON.stringify(config, null, 2), (e) => {
+              if (e) reject(e);
+
+              resolve(config);
+            });
+          });
+      });
+    }
+  });
 }
 
 const file1 = fs.readFileSync(process.argv[2], 'utf-8');
 const file2 = fs.readFileSync(process.argv[3], 'utf-8');
 
-request
-  .post('https://diffchecker-api-production.herokuapp.com/diffs')
-  .send({
-    left: file1,
-    right: file2,
-    expiry: 'hour',
-    title: null
-  })
-  .end( (error, response) => {
-    if (error) {
-      console.log(error);
-      process.exit(1);
-    }
+authorization()
+.then(config => {
+  request
+    .post(API_URI + '/diffs')
+    .set('Authorization', 'Bearer ' + config.authToken)
+    .send({
+      left: file1,
+      right: file2,
+      expiry: 'hour',
+      title: null
+    })
+    .end((error, response) => {
+      if (error) {
+        console.log(error);
+        process.exit(1);
+      }
 
-    const url = 'http://diffchecker.com/' + response.body.slug;
+      const url = 'https://diffchecker.com/' + response.body.slug;
 
-    console.log(url + '\n');
-    opener(url);
-    process.exit();
-  });
+      console.log('Diff created at: ' + url);
+      opener(url);
+      process.exit();
+    });
+})
+.catch(error => {
+  console.log(error);
+});
